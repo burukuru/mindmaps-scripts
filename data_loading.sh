6116633468eecc601035c9f0fab11fad825cea87
Thanh C Pham <thanh@mindmaps.io>
2016-09-29 00:52:21 +0100

    Rewrite for distributed loading, etc.
    Load everything from single script (no need for data_logger.sh.)
    Supports loading ontology and data separately.
    Automatically picks up the log file (might need lsof + permissions)

--- data_loading.sh
+++ data_loading.sh
@@ -1,51 +1,66 @@
-TMP=`echo $PWD`
-SCHEMA=\'\{\"path\":\"$TMP/schema.gql\"\}\'
-DATA=\'\{\"path\":\"$TMP/movie-data.gql\"\}\'
+#!/bin/bash
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(readlink -m $(dirname $0))
 
+LOAD_COMPONENT=all
+if [[ $1 == ontology ]]; then
+  LOAD_COMPONENT=ontology
+elif [[ $1 == data ]]; then
+  LOAD_COMPONENT=data
+fi
+
+SCHEMA_DIR="$PROGDIR/sample-datasets/movies/"
+DATA_GQL_DIR="$PROGDIR/sample-datasets/movies/small-dataset/"
+SLAVE_NODES=$(cat $PROGDIR/engine_slave_dev_mindmaps.txt)
+
+SCHEMA="{\"path\":\"$SCHEMA_DIR/schema.gql\"}"
+GQL_DATA="{\"path\":\"$DATA_GQL_DIR/large-dataset-data.gql\",\"hosts\":[${SLAVE_NODES}]}"
 WAITING=true
+LOADING=true
+
+MINDMAPS_LOG=$(lsof -p $(cat /tmp/mindmaps-engine.pid) | grep mindmaps.log | awk '{print $9}')
 
 echo $$ > /tmp/data_loader.pid
-trap 'WAITING=false' SIGUSR1
-### THE FOLLOWING COMMANDS MUST BE EXECUTED IN ORDER!
-## Instructions: Uncomment each of the following lines one at a time, in order
-## and execute everytime this script from the folder where the Movie .gql files are located
-echo "Waiting for the logger to start"
-while $WAITING;
-do
-	sleep 1
-done
 sleep 1
-LOGGERPID=`cat /tmp/data_logger.pid`
-echo "Logger started"
-echo "Loading the ontology"
-eval curl -H \"Content-Type: application/json\" -X POST -d `echo $SCHEMA` http://localhost:4567/import/ontology
 
-while $WAITING;
-do
-	sleep 1
-done
-echo "Ontology loaded"
-WAITING=true
-echo "Loading data"
-eval curl -H \"Content-Type: application/json\" -X POST -d `echo $DATA` http://localhost:4567/import/batch/data
-sleep 3
-kill -SIGUSR2 $LOGGERPID
-while $WAITING;
-do
-	sleep 1
-done
+if [[ $LOAD_COMPONENT == "all" || $LOAD_COMPONENT == "ontology" ]]; then
+  echo "Loading the ontology"
+  curl -s -H "Content-Type: application/json" -X POST -d ${SCHEMA} http://localhost:4567/import/ontology
 
-kill -SIGUSR2 $LOGGERPID
+  while $LOADING;
+  do
+    (tail -n1 -f ${MINDMAPS_LOG} & ) | grep -q -e "Ontology loaded" -e "All tasks done" && LOADING=false && WAITING=false
+    while $WAITING;
+    do
+      sleep 2
+    done
+  done
+  echo ""
+  echo "Ontology loaded"
+fi
 
-while $WAITING;
-do
-	sleep 1
-done
+LOADING=true
+COUNT=0
+
+if [[ $LOAD_COMPONENT == "all" || $LOAD_COMPONENT == "data" ]]; then
+  echo "Loading data"
+  curl -s -H "Content-Type: application/json" -X POST -d ${GQL_DATA} http://localhost:4567/import/distribute/data
+  sleep 3
+  while $LOADING;
+  do
+    (tail -n0 -f ${MINDMAPS_LOG} & ) | grep -q -e "All tasks done" && let COUNT+=1
+    if [[ $COUNT -ge 2 ]]; then
+      LOADING=false
+    fi
+  done
+  echo ""
+  echo "Data loaded"
+fi
+
 rm /tmp/data_loader.pid
 
-kill -SIGUSR1 $LOGGERPID
-kill -SIGUSR2 $LOGGERPID
+echo ""
 echo "Everything loaded! You can start Graql now!"
 exit 0
-#Ontology loaded
-#All tasks done!
+##Ontology loaded
+##All tasks done!
